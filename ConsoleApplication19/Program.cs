124f399629d85b28cfce26a730b7eed894f31b78
Amin Khalighi Sikaroodi <a.k.sika@live.com>
2017-01-15 12:00:53 +0330

    Add files via upload

---
+++ ConsoleApplication19/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Head head = new Head();
+            head.color = "green";
+            Tongue tongue = new Tongue();
+            tongue.color = "red";
+            if (tongue.color == "red" && head.color == "green")
+            {
+                head.cut();
+            }
+        }
+
+        class Head
+        {
+            
+            public string color;
+            public void cut()
+            {
+                
+            }
+        }
+
+        class Tongue
+        {
+            public string color;
+            void speak()
+            {
+
+            }
+        }
+
+    }
+}
